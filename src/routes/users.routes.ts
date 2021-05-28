c3f02fcd7111686d8b46fd0ed1f1302f74793397
wilgnerschuertz <wilgnerschuertz@outlook.com.br>
2021-05-28 09:40:16 -0400

    Teste
    Teste

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -39,7 +39,10 @@ usersRouter.patch(
         user_id: request.user.id,
         avatarFilename: request.file.filename,
       });
-      delete user.password;
+
+      //Remove o password do response.json(user)
+      // delete user.password;
+
       return response.json(user);
     } catch (err) {
       return response.status(400).json({ error: err.message });
